2ea4049eea4cfa1d025cffa5c6e6d6f58babb7d3
Alexander Cecile <35971201+AlexanderCecile@users.noreply.github.com>
2020-02-14 00:11:46 -0500

    Initial recursive procedure for 1.11
    Created recursive procedure for exercise 1.11.

--- exercises/chapter-1/1.11/src.rkt
+++ exercises/chapter-1/1.11/src.rkt
@@ -1,1 +1,16 @@
 #lang sicp
+
+(define (f-recur n)
+  (if (< n 3) n
+      (+
+       (f-recur (- n 1))
+       (* 2 (f-recur (- n 2)))
+       (* 3 (f-recur (- n 3))))))
+
+#| a comment |#
+
+(define (f-iter n)
+  796)
+
+(display (= (f-recur 9) (f-iter 9) 796))
+(newline)
